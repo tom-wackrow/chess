0be6e48aa9d491277b9a7e823a77237ca1a24e4f
tommytank20 <wackrowtom@gmail.com>
2022-12-14 10:39:00 +0000

    pass & play chess implemented

--- static/js/chess.js
+++ static/js/chess.js
@@ -1,1 +1,32 @@
-var board = Chessboard("board", "start")
+var config = {
+    draggable: true,
+    position: "start",
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+}
+
+var board = Chessboard("board", config)
+var game = new Chess()
+
+
+function onDragStart (source, piece, position, orientation) {
+    // if the game is over, do not pick up any pieces
+    if (game.game_over()) return false
+
+    // only pick up pieces for the side whose turn it is
+    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+    return false
+    }
+}
+
+function onDrop(source, target) {
+    // move will be null if move is not legal
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: "q", // for now always promote to queen
+    })
+    
+    if (move === null) return "snapback" // if move is not legal, snap back to previous board state
+}
